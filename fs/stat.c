423e06c40a50915a74d53ef2a94ab30691c6402f
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-08-18 11:51:50 +0700

    kernel: Add SUSFS inline hook patches for KSU
    Applied susfs_inline_hook_patches.sh (v2.2.00) for kernel 4.4
    
    Patched files:
    - fs/exec.c: execveat hook with su compat
    - fs/open.c: faccessat hook
    - fs/read_write.c: sys_read hook for init.rc
    - fs/stat.c: vfs_fstat/stat hook
    - drivers/input/input.c: input event hook
    - security/selinux/hooks.c: SELinux bprm check
    - security/selinux/ss/services.c: policy_rwlock export
    - kernel/reboot.c: sys_reboot hook
    - include/linux/seccomp.h: filter_count field
    
    Skipped (not needed for 4.4):
    - fs/namei.c: throne_tracker
    - security/security.c: setuid hook
    - kernel/sys.c: setresuid hook

--- fs/stat.c
+++ fs/stat.c
@@ -17,6 +17,9 @@
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 extern void susfs_sus_kstat_spoof_generic_fillattr(struct inode *inode, struct kstat *stat);
@@ -97,6 +100,11 @@ int vfs_getattr(struct path *path, struct kstat *stat)
 
 EXPORT_SYMBOL(vfs_getattr);
 
+#ifdef CONFIG_KSU_SUSFS
+extern struct static_key_true ksu_is_init_rc_hook_enabled;
+extern void ksu_handle_vfs_fstat(int fd, loff_t *kstat_size_ptr);
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 int vfs_fstat(unsigned int fd, struct kstat *stat)
 {
 	struct fd f = fdget_raw(fd);
@@ -104,11 +112,22 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 
 	if (f.file) {
 		error = vfs_getattr(&f.file->f_path, stat);
+#ifdef CONFIG_KSU_SUSFS
+		if (static_branch_unlikely(&ksu_is_init_rc_hook_enabled))
+			ksu_handle_vfs_fstat(fd, &stat->size);
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 		fdput(f);
 	}
 	return error;
 }
 EXPORT_SYMBOL(vfs_fstat);
+
+#ifdef CONFIG_KSU_SUSFS
+extern struct static_key_true ksu_su_compat_enabled;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
 
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
@@ -116,6 +135,16 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
+
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted()))
+		goto orig_flow;
+	if (static_branch_likely(&ksu_su_compat_enabled)) {
+		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val)))
+			ksu_handle_stat(&dfd, &filename, &flag);
+	}
+orig_flow:
+#endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
